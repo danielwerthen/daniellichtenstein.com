063ebcbec59a684688f45cb53f32a478a3ce15a7
Daniel Werthen <danielwerthen@gmail.com>
2020-02-27 13:45:36 +0100

    Initial outline

---
+++ src/pages/Frontpage.js
@@ -0,0 +1,12 @@
+import React from 'stilren/react';
+
+const blue = '#61DAFB';
+export default function Frontpage() {
+  return (
+    <div $color={blue}>
+      <div $maxWidth="1024px" $margin="auto">
+        <div>Daniel Lichtenstein</div>
+      </div>
+    </div>
+  );
+}
